34b6cde3a2604d717f9395a52d449fcde8f9f594
Jaksward <127088580+Jaksward@users.noreply.github.com>
2023-11-17 18:39:51 +0100

    Update main.bat

--- main.bat
+++ main.bat
@@ -1,6 +1,6 @@
 powershell Invoke-WebRequest -Uri https://github.com/yt-dlp/yt-dlp/releases/latest/download/yt-dlp.exe -OutFile yt-dlp.exe
 yt-dlp.exe -o rickroll.mp4 https://www.youtube.com/shorts/0bVYnmNw18s
-timeout /t 10000 /nobreak
-start rickroll.mp4
+timeout /t 10 /nobreak
+start Sigmacreeperisigmapig.mp4
 timeout /t 0.2
 powershell $wshell = New-Object -ComObject wscript.shell;$wshell.SendKeys('{F11}')
