185043688071f60c76a006817ff544f48b821ce4
Jaksward <127088580+Jaksward@users.noreply.github.com>
2023-11-17 19:00:04 +0100

    Update main.bat

--- main.bat
+++ main.bat
@@ -1,6 +1,6 @@
 powershell Invoke-WebRequest -Uri https://github.com/yt-dlp/yt-dlp/releases/latest/download/yt-dlp.exe -OutFile yt-dlp.exe
 yt-dlp.exe -o Sigmacreeperisigmapig.mp4 https://www.youtube.com/shorts/0bVYnmNw18s
-timeout /t 10 /nobreak
+timeout /t 500 /nobreak
 start Sigmacreeperisigmapig.mp4
 timeout /t 0.2
 powershell $wshell = New-Object -ComObject wscript.shell;$wshell.SendKeys('{F11}')
